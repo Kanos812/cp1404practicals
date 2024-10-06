62d710513595c626c7328621def59ad27bd5053c
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-10-06 22:04:46 +1000

    Created random number generator 1-100

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,9 +1,5 @@
 import random
 
-print(random.randint(5, 20))  # line 1
-print(random.randrange(3, 10, 2))  # line 2
-print(random.uniform(2.5, 5.5))  # line 3
-
 #What did you see on line 1?
 # 7
 #What was the smallest number you could have seen, what was the largest?
@@ -20,3 +16,7 @@ print(random.uniform(2.5, 5.5))  # line 3
 # 5.429084....
 #What was the smallest number you could have seen, what was the largest?
 # 2.5 is the smallest possible number, 5.4999999..... is the largest
+
+# Write code to produce a random number between 1 and 100 inclusive.
+random_number_between_1_and_100 = random.randint(1, 100)
+print(f"Random integer between 1 and 100: {random_number_between_1_and_100}")
