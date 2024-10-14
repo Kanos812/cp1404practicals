44263d0302080bde03d8af4019d96c27608b19a9
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-10-14 22:17:11 +1000

    Created lottery quick pick function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,6 +1,21 @@
 import random
 
-NUMBERS_PER_LINE = 6
-MINIMUM = 1
-MAXIMUM = 45
+numbers_per_line = 6
+minimum = 1
+maximum = 45
 
+def main():
+    """Generates lottery quick picks."""
+    number_of_quick_picks = int(input("How many quick picks? "))
+
+    for _ in range(number_of_quick_picks):
+        quick_pick = []
+        while len(quick_pick) < numbers_per_line:
+            number = random.randint(minimum, maximum)
+            if number not in quick_pick:
+                quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+if __name__ == "__main__":
+    main()
