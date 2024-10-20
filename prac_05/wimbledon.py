5600dc8901a2c86299dd15ec9bcb36fb0b7ab221
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-10-20 21:43:41 +1000

    Added countries that have won wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,12 +1,14 @@
 def main():
-    """Read data from CSV file and display processed information."""
+    """ Read data from CSV file and display processed information """
     filename = "wimbledon.csv"
     champions_data = load_data(filename)
 
     display_champions(champions_data)
+    display_countries(champions_data)
+
 
 def load_data(filename):
-    """Read CSV file and return a list of lists."""
+    """ Read CSV file and return a list of lists """
     data = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         for line in in_file:
@@ -14,16 +16,30 @@ def load_data(filename):
             data.append(parts)
     return data
 
+
 def display_champions(data):
-    """Display champions and their win counts."""
+    """ Display champions and their win counts """
     champions_wins = {}
     for row in data[1:]:
         champion = row[2]
         champions_wins[champion] = champions_wins.get(champion, 0) + 1
 
-    print("Wimbledon Champions: ")
+    print("Wimbledon Champions:")
     for champion, wins in champions_wins.items():
         print(f"{champion} {wins}")
+
+
+def display_countries(data):
+    """ Display countries of champions in alphabetical order """
+    countries = set()
+    for row in data[1:]:
+        country = row[1]
+        countries.add(country)
+
+    countries_list = sorted(list(countries))
+    print("\nThese", len(countries_list), "countries have won Wimbledon:")
+    print(", ".join(countries_list))
+
 
 if __name__ == "__main__":
     main()
