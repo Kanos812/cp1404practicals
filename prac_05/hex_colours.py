f21c21d8989918abf31c1f82890adc53821c4a6d
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-10-20 18:08:13 +1000

    Added dictionary for colour codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,13 @@
+COLOR_CODES = {
+    "crimson": "#DC143C",
+    "teal": "#008080",
+    "coral": "#FF7F50",
+    "slategray": "#708090",
+    "turquoise": "#40E0D0",
+    "goldenrod": "#DAA520",
+    "lavender": "#E6E6FA",
+    "royalblue": "#4169E1",
+    "tomato": "#FF6347",
+    "sienna": "#A0522D"
+}
+
