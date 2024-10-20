5109063bc0d6d4f456214d0ec9c6ef09ef9e8c65
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-10-20 17:59:28 +1000

    Prints all states & abbreviations ; Change exception format to 'EAFP'

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,12 +1,18 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
 """
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
+
+# Use for loop to interate through 'CODE_TO_NAME' dictionary and print formatted
+
+for state_code, state_name in CODE_TO_NAME.items():
+    print(f"{state_code:3} is {state_name}")
+
+# User input for conversion
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
@@ -15,3 +21,5 @@ while state_code != "":
     except:
         print("Invalid short state")
     state_code = input("Enter short state: ")
+
+
