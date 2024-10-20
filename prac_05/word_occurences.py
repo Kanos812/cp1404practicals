920d7ba30f70c94e58286b73fad65dd6fc66952a
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-10-20 18:29:06 +1000

    Initial loop to count no of words in sentence because I misread the previous instructions

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,17 +1,18 @@
 def main():
     # Count Occurrences of Words in String
+
     text = input("Text: ")
     words = text.split()
     word_counts = {}
     for word in words:
         if word in word_counts:
-            word_counts[word] += 1
+            word_counts[word] += 1  # Increment count if word already exists
         else:
-            word_counts[word] = 1
+            word_counts[word] = 1   # Initialize count if word is new
 
-    # Display words with associated no of characters
-    for word in word_counts:
-        print(f"{word} : {len(word)}")
+    # Display words and their frequencies
+    for word, count in word_counts.items():
+        print(f"{word} : {count}")  # Print word and its count
 
 if __name__ == "__main__":
     main()
