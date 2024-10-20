e396d06fdd7fba5610a4ccb421b13c38f19fc2ba
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-10-20 18:03:44 +1000

    Allows lower case input when spelling abbreviation

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -10,14 +10,14 @@ print(CODE_TO_NAME)
 # Use for loop to interate through 'CODE_TO_NAME' dictionary and print formatted
 
 for state_code, state_name in CODE_TO_NAME.items():
-    print(f"{state_code:3} is {state_name}")
+    print(f"{state_code} is {state_name}")
 
 # User input for conversion
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     try:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+        print(state_code, "is", CODE_TO_NAME[state_code.upper()])
     except:
         print("Invalid short state")
     state_code = input("Enter short state: ")
