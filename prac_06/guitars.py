f60fa24dfd454d7b80aa8f252cd16afe04e9308c
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-03 19:07:04 +1000

    Displays guitar details

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -14,5 +14,12 @@ def main():
         print(guitar, "added")
         name = input("Name: ")
 
+    # Display guitar details
+    print("\n... snip ...\n")
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitar, 1):
+        vintage_string = " (vintage) " if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
 if __name__ == "__main__":
     main()
