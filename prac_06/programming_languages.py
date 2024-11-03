9d92ff23c52009e79be66781b83b0dfb3229160a
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-03 17:53:22 +1000

    Create is_dynamic function to check if language is dynamically typed ; Create __str__ to return string representation of language parameters

--- prac_06/programming_languages.py
+++ prac_06/programming_languages.py
@@ -17,3 +17,11 @@ def __init__(self, name="", typing="", reflection="", year=""):
     self.typing = typing
     self.reflection = reflection
     self.year = year
+
+def is_dynamic(self):
+    """Determine if language is dynamically typed."""
+    return self.typing == "Dynamic"
+
+def __str__(self):
+    """Return string representation of language."""
+    return f"{self.name}, {self.typing} Typing, Reflection = {self.reflection}, First appeared in {self.year}"
