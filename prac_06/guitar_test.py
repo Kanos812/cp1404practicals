e372dbf2b0fa9bbe7042dbcde8f195ac2376dfe8
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-03 18:49:40 +1000

    Initial test for get_age

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -5,5 +5,9 @@ def main():
     guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
     guitar2 = Guitar("Another Guitar", 2013, 1000)
 
+    # Test get_age method
+    print("Gibson L-5 CES get_age() - Expected 100. Got", guitar1.get_age())
+    print("Another Guitar get_age() - Expected 9. Got", guitar2.get_age())
+
 if __name__ == "__main__":
     main()
