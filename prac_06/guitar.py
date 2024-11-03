abc560d1ef13522aebe8eadaa29a07a8c945d040
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-03 18:40:43 +1000

    Define __str__ and create function to get age of guitar

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -9,10 +9,20 @@ Actual Completion -
 """
 
 class Guitar:
-    """Store details of guitar"""
+    """Store details of guitar."""
 
     def __init__(self, name="", year=0, cost=0):
-        """Initialise guitar object"""
+        """Initialise guitar object."""
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return string representation of a Guitar."""
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        """Get the age of the guitar in years."""
+        # Calculate age based on current year (replace with actual current year)
+        current_year = 2023
+        return current_year - self.year
