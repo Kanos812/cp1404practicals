b00ef958cc5acb579121364a3a6ee8fefe560d5f
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-03 16:42:56 +1000

    Updated objects to include their name

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -8,17 +8,15 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
     """ Limo Car Class """
-    limo = Car(fuel=100) #Add Limo
+    limo = Car("Limo", 100) #Add Limo
     limo.add_fuel(20) # Add 20 units of fuel to Limo
-    print(f"Limo has fuel: {limo.fuel}")
     limo.drive(115) # Drive 115 units
-    print("Odometer =", limo._odometer) #Display odometer
     print(limo)
 
 main()
