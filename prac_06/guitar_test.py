ff2399880da66b9fe73fc6353fbf893763a5d580
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-03 18:46:43 +1000

    Defined main function & defined guitar classes

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,9 @@
+from guitar import Guitar
+
+def main():
+    """Test the Guitar class."""
+    guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    guitar2 = Guitar("Another Guitar", 2013, 1000)
+
+if __name__ == "__main__":
+    main()
