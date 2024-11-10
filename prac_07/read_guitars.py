754ecdb4181045393b9bb3df12c56558c4c6c2e0
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-10 22:22:31 +1000

    Create main() function

--- prac_07/read_guitars.py
+++ prac_07/read_guitars.py
@@ -4,3 +4,26 @@ File Reader for guitars.csv - opens/reads a file, stores in objects of custom cl
 Harrison O'Kane
 """
 
+from guitar import Guitar
+
+def main():
+    """Read guitar data from file and store in list of Guitar objects."""
+
+    guitars = [] #Create an initially empty list
+
+    try:
+        with open('guitars.csv', 'r') as file: #Open file to be read
+            for line in file:
+                parts = line.strip().split(',') #Split line into parts
+                name = parts[0]
+                year = int(parts[1])
+                cost = int(parts[2])
+
+                guitar = Guitar(name, year, cost) #Create guitar object
+                guitars.append(guitar) #Add created object to list
+
+    except FileNotFoundError:
+        print('Error - File not Found') #Error handling in case of missing file
+
+if __name__ == '__main__':
+    main()
