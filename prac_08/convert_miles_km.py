63fb16709de0713b0c45df555508a2c0b6e07f68
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-17 16:21:36 +1000

    Created py code that converts miles to km and displays result

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,7 @@
+conversion_factor = 1.609344 #Mile to Kilometres
+
+miles = float(input("Please enter distance in miles: ")) #Get user input
+kilometers = miles * conversion_factor #Perform conversion calculation
+
+print(f"{miles} mi is equal to {kilometers} km.") #Display result
+
