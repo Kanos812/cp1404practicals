1ea98adba47b21b10747f54dca29573896e1b074
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-17 15:32:53 +1000

    Added callback that greets user when 'greet' button is pressed

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,8 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("Greetings!") #Greet the usser
+
 
 BoxLayoutDemo().run()
