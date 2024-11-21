540ecccd3fda9e653b1ae3c341576d3e1306a6fc
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-21 16:39:59 +1000

    Created __init__ for new UnreliableCar class

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -4,3 +4,14 @@ Unreliable Car
 Harrison O'Kane
 21/11/2024
 """
+
+from prac_09.car import Car
+import random
+
+class UnreliableCar(Car):
+    "Create a class for an Unreliable Car example."
+
+    def __inti__(self, name, fuel, reliability):
+        """Initialise UnreliableCar instance."""
+        super().__init__(name,fuel)
+        self.reliability = reliability
