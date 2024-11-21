c86f4c6ba8404ec5b074de301f6a7e7a0ee190cb
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-21 16:14:09 +1000

    Completed walkthrough example - taxi meter resets and completes second trip

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,7 +1,13 @@
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi(name="Prius 1", fuel=100, price_per_km=1.23)
-my_taxi.drive(40)
+my_taxi = Taxi(name="Prius 1", fuel=100, price_per_km=1.23) #Define taxi attributes
+my_taxi.drive(40) #Drive 40km
+
+print(my_taxi) #Print taxi attributes
+print(f"Current fare: ${my_taxi.get_fare():.2f}") #Display the current calculated fare
+
+my_taxi.start_fare() #Restarting meter
+my_taxi.drive(100)
 
 print(my_taxi)
-print(f"Current fare: ${my_taxi.get_fare():.2f}")
+print(f"Current fare: ${my_taxi.get_fare():.2f}") #Print details again after second trip
