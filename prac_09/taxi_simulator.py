5454a219808f9ef2373cff1b987e8d316abfd43c
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-22 04:14:05 +1000

    Converted loop into dictionary to improve readability and efficiency

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -17,48 +17,33 @@ menu_choice = ""  # Initialise menu choice
 
 def display_taxis(taxis):
     """Display a numbered list of taxis."""
-    for i, Taxi in enumerate(taxis):  #Iterate over taxis list
-        print(f"{i} - {Taxi}")
+    for i, taxi in enumerate(taxis):  #Iterate over taxis list
+        print(f"{i} - {taxi}")
 
-#TODO: Clean up - not functioning properly
+"""Menu Directory."""
+menu = {
+    "q": {"description": "Quit", "function": None},  # Quit has no function
+    "c": {"description": "Choose taxi", "function": choose_taxi},
+    "d": {"description": "Drive", "function": drive_taxi},
+}
 
-while menu_choice != "q":
+while True:
     print("Please select an Option: "
-          "\n (q)uit"
-          "\n (c)hoose a taxi "
-          "\n (d)rive taxi")
-    menu_choice = input(">>>").lower()
-    if menu_choice == "c":
-        print("Taxis available: ")
-        display_taxis(Taxi)
-        taxi_choice = int(input("Please choose a taxi: "))
-        try:
-            current_taxi = taxis[taxi_choice]
-        except IndexError:
-            print("Invalid taxi choice")
-    elif menu_choice == "d":
-        if current_taxi:
-            distance = float(input("Drive how far? "))
-            current_taxi.start_fare()
-            current_taxi.drive(distance)
-            trip_cost = current_taxi.get_fare()
-            print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
-            total_bill += trip_cost
+          "\n (Q)uit"
+          "\n (C)hoose a taxi "
+          "\n (D)rive taxi")
+    menu_choice = input(">>> ").lower()
+    if menu_choice in menu:
+        if menu_choice == "q":
+            print("Program Exiting. Thank You...")
+            break  # Exit loop if choice is 'q'
         else:
-            print("You need to choose a taxi before you can drive")
-    elif menu_choice == "q":
-        print("Program exiting. Thank you.")
-        break
+            action = menu[menu_choice]["function"]
+            if action:
+                if action == choose_taxi:
+                    current_taxi = action(taxis)  # Pass taxis to choose_taxi
+                else:
+                    trip_cost = action(current_taxi)  # Pass current_taxi to drive_taxi
+                    total_bill += trip_cost
     else:
         print("Invalid option")
-
-#TODO: Convert to dictionary???
-
-# Final Output
-# Print total trip cost message
-# Print "Taxis are now:"
-# Display taxis using display_taxis function
-
-# display_taxis function
-# For each taxi in the taxis list:
-# Print taxi number and details using __str__ method
