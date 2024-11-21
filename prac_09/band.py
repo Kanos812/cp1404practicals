ee2d8c35f33f0534c508db129ef9cd69acf7c0a1
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-22 00:51:46 +1000

    Created pseudocode

---
+++ prac_09/band.py
@@ -0,0 +1,20 @@
+# Class Band:
+#     Attributes:
+#         name (string): The name of the band.
+#         musicians (list): A list of Musician objects in the band.
+
+#     Methods:
+#         __init__(name):
+#             - Initialize the band with the given name.
+#             - Create an empty list to store musicians.
+
+#         __str__():
+#             - Return a string representation of the band, including its name and a list of musicians.
+
+#         add(musician):
+#             - Add the given musician to the band's list of musicians.
+
+#         play():
+#             - Get the playing status of each musician in the band.
+#             - Combine the statuses into a multi-line string, separated by newlines.
+#             - Return the combined string.
