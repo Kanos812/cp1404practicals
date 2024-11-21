646e176c01f2958fd7d27fdb5f7164afb375205e
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-21 17:02:57 +1000

    Definite __init__ for new SilverServiceTaxi class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,3 +5,12 @@ Harrison O'Kane
 21/11/2024
 """
 
+from prac_09.car import Car
+
+class SilverServiceTaxi(Car):
+    """Initialise SilverServiceTaxi instance."""
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = 1.23 * fanciness
+
