dfc9c811b05b074bb91cf7b01055fd65369d1ec9
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-22 01:12:47 +1000

    Formatted play and _str_ functions to give desired output

--- prac_09/band.py
+++ prac_09/band.py
@@ -4,8 +4,8 @@ Band Class
 Harrison O'Kane
 21/11/2024
 """
-from prac_09 import musician
 
+import musician
 
 class Band:
     """Band class to manage collection of musicians."""
@@ -17,12 +17,12 @@ class Band:
 
     def _str_(self):
         """Return string representation of band collection."""
-        return f"{self.name}"
+        return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
 
     def add(self, musician):
         """Add a musician."""
         self.musicians.append(musician)
 
     def play(self):
-        "Return a string of each musician and their instrument."
-        return"Test"
+        "Return a string of each musician and their instrument (if available)."
+        return '\n'.join(musician.play() for musician in self.musicians)
