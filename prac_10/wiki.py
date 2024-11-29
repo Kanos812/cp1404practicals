e1440c4acb24b9d60ea23f5b61695a2ce6806d80
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-30 06:22:45 +1000

    Create pseudocode to plan structure

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -5,3 +5,32 @@ Prompts user for page title and prints page details
 Estimated Time to Complete - 0:30
 Actual Time to Complete -
 """
+
+import wikipedia
+
+"""
+FUNCTION fetch_wiki_page(title):
+    TRY:
+        page = GET Wikipedia page with title (autosuggest=False)
+        RETURN page
+    EXCEPT DisambiguationError AS e:
+        PRINT "Disambiguation error: " + e.options
+    EXCEPT PageError:
+        PRINT "Page not found for title: " + title
+    EXCEPT Exception AS e:
+        PRINT "An error occurred: " + e
+
+FUNCTION main():
+    WHILE True:
+        title = INPUT "Enter a Wikipedia page title or search phrase (or leave blank to exit): "
+        IF title IS empty:
+            BREAK
+        page = CALL fetch_wiki_page(title)
+        IF page IS NOT None:
+            PRINT "Title: " + page.title
+            PRINT "Summary: " + page.summary
+            PRINT "Content: " + first 500 characters of page.content
+            PRINT separator line
+
+CALL main()
+"""
